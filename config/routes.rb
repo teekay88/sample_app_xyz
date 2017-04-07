bbf90f8add8b9de2abc3ab64df7e0dedfcf21f8c
teekay88 <takilein@gmx.de>
2017-04-07 12:09:54 +0200

    User model and secure password

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+  #creates two names routes help_path := /help and help_url := www.localhost/help
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+end
