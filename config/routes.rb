ee0f30286270e9461ac63d393bfb1737f9e092e6
teekay88 <takilein@gmx.de>
2017-04-08 09:58:55 +0200

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   root 'static_pages#home'
   #creates two names routes help_path := /help and help_url := www.localhost/help
   get  '/help',    to: 'static_pages#help'
@@ -8,5 +7,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
