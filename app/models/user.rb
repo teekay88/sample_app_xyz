01dc145555f1834431b18a7e56224112e91baac9
teekay88 <takilein@gmx.de>
2017-04-09 21:42:32 +0200

    account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  # method reference; calls the method downcase_email
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,6 +10,8 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -19,6 +23,16 @@ class User < ApplicationRecord
   # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # Activates an account.
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   # Remembers a user in the database for use in persistent sessions.
@@ -33,8 +47,31 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+     # Returns true if the given token matches the digest.
+   def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+   end
+
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+  # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+  # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+ 
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
     'Cache-Control' => 'public, max-age=3600'
   }
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
